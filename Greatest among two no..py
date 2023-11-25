a0927a129e86dd4ed54f3db3128d06fd310f8bbc
utkarsh00021 <142799308+utkarsh00021@users.noreply.github.com>
2023-11-25 13:13:10 +0530

    Add files via upload

---
+++ Greatest among two no..py
@@ -0,0 +1,8 @@
+a=float(input("enter first number:"))
+b=float(input("enter second number"))
+if(a==b):
+    print("both numbers are same")
+elif(a>b):
+    print("a is greater then b")
+else:
+    print("a is smaller then b")
