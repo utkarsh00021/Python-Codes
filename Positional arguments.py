911d8f028fadc1e0d233135f7e64144f2cea390a
utkarsh00021 <142799308+utkarsh00021@users.noreply.github.com>
2023-11-30 21:57:50 +0530

    Add files via upload

---
+++ Positional arguments.py
@@ -0,0 +1,9 @@
+def display_info(name,rolln,sec,cllg):
+    data=f'''
+    Name:{name}
+    Section:{sec}
+    Roll no.:{rolln}
+    college:{cllg}'''
+    return data
+out=display_info('Utkarsh',56,'CD','GLA')
+print(out)
